f55162028216baee4a31bad52592ea385fd92a53
qgarot <quentingarot06@gmail.com>
2024-12-31 00:08:29 +0100

    Very simple registration check

--- src/main/java/org/cryptoserver/packets/authentication/RegisterEvent.java
+++ src/main/java/org/cryptoserver/packets/authentication/RegisterEvent.java
@@ -1,6 +1,7 @@
 package org.cryptoserver.packets.authentication;
 
 import org.cryptoserver.packets.Event;
+import org.cryptoserver.packets.headers.OutgoingHeaders;
 import org.cryptoserver.server.Connection;
 import org.cryptoserver.users.UserManager;
 import org.json.JSONObject;
@@ -13,10 +14,34 @@ public class RegisterEvent extends Event {
         String password = packet.getString("password");
         String confirmPassword = packet.getString("confirmPassword");
 
-        if (password.equals(confirmPassword)) {
-            if (!UserManager.getInstance().getUserDetailsDao().isRegistered(username)) {
-                UserManager.getInstance().getUserDetailsDao().insert(username, "", password);
+        JSONObject response = new JSONObject();
+        response.put("header", OutgoingHeaders.REGISTER_RESPONSE);
+
+        System.out.println(username);
+
+        if (!username.isEmpty() && !password.isEmpty() && !confirmPassword.isEmpty()) {
+            if (password.equals(confirmPassword)) {
+                if (!UserManager.getInstance().getUserDetailsDao().isRegistered(username)) {
+                    // Register user in database
+                    UserManager.getInstance().getUserDetailsDao().insert(username, "", password);
+                    // Success message
+                    response.put("success", true);
+                    response.put("message", "Inscription validée ! Retournez sur le formulaire de connexion");
+                    connection.sendPacket(response);
+                } else {
+                    response.put("success", false);
+                    response.put("message", "Ce nom d'utilisateur est déjà pris");
+                    connection.sendPacket(response);
+                }
+            } else {
+                response.put("success", false);
+                response.put("message", "Les mots de passe ne correspondent pas");
+                connection.sendPacket(response);
             }
+        } else {
+            response.put("success", false);
+            response.put("message", "Tous les champs doivent être remplis");
+            connection.sendPacket(response);
         }
     }
 }
--- src/main/java/org/cryptoserver/packets/headers/OutgoingHeaders.java
+++ src/main/java/org/cryptoserver/packets/headers/OutgoingHeaders.java
@@ -3,4 +3,5 @@ package org.cryptoserver.packets.headers;
 public class OutgoingHeaders {
     public static final int LOGIN_RESPONSE = 1;
     public static final int DASHBOARD_SELECT_CRYPTO_RESPONSE = 2;
+    public static final int REGISTER_RESPONSE = 3;
 }
